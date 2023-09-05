5db285d7bdae5fffc638a6f88a930fc3d91a1532
borzX <1985borz@mail.ru>
2023-09-05 13:03:24 +0300

    hw10

---
+++ hw10.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human'] = '0'
+lst_len = len(lst)
+data.head(lst_len) 
+print(data)
